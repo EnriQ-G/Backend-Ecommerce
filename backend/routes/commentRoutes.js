9cd14e9c44d09fba98c83383c2bd2111fb022ea2
Enrique Graniel <legraniel.91@gmail.com>
2023-10-03 19:39:24 -0600

    modificación para traer todos los comentarios de una pelicula

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router()
 const { getComments, setComment, deleteComment } = require('../controllers/commentController')
 const { protect } = require('../middleware/authMiddleware')
 
-router.get('/', getComments)
+router.get('/:id', getComments)
 router.post('/:id', protect, setComment)
-
 router.delete('/:id', protect, deleteComment)
 
 module.exports = router
