68ddc91b4d620562a97f8b7481b6edabd9e1a6eb
Enrique Graniel <legraniel.91@gmail.com>
2023-10-05 17:58:17 -0600

    agregar función protect al endpoint de creación de comentarios

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const { getComments, setComment, deleteComment } = require('../controllers/commentController')
 const { protect } = require('../middleware/authMiddleware')
 
-router.get('/:id', protect, getComments)
-router.post('/', setComment)
+router.get('/:id', getComments)
+router.post('/', protect, setComment)
 router.delete('/:id', deleteComment)
 
 module.exports = router
